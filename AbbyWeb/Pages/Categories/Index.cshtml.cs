6257c74e7fc24013077575dfe98ae4e747305058
SambathRanchaya <SambathRanchaya@DESKTOP-2E39V1G>
2022-07-12 10:41:36 -0500

    Add project files.

---
+++ AbbyWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDBContext _db;
+
+        public IEnumerable<Category> Categories { get; set; }
+        public IndexModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            Categories = _db.Category;
+        }
+    }
+}
